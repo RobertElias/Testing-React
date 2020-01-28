49e6d36afc4ca1e4deb457e93f6a2bfa8cfce061
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-27 17:49:16 -0800

    test passed met MVP

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++ star-wars-app/src/components/StarWarsCharacter.test.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import {getData as mockGetData} from '../api';
 import StarWarsCharacters from './StarWarsCharacters';
 
-test('renders character names', async ()=> {
-    //AAA Arrange, Act, Assert
-    const { getByLabelText, getByText} = render(<StarWarsCharacters/>);
 
-    //const characterName = getByLabelText(/name/i);
+jest.mock('../api');
 
-    const prevButton = getByText(/previous/i);
-    const nextButton = getByText(/next/i);
-
-    fireEvent.click(prevButton);
-    fireEvent.click(nextButton);
+// test('renders button by text', ()=> {
+//     //AAA Arrange, Act, Assert
+//     const { getByText} = render(<StarWarsCharacters/>);
 
 
+//     const prevButton = getByText(/previous/i);
+//     const nextButton = getByText(/next/i);
+
+//     fireEvent.click(prevButton);
+//     fireEvent.click(nextButton);
+
+
+// });
+
+test('test if API is working', async ()=> {
+
+    const someData = {
+        next: "test",
+        results: [
+            {
+                name: "Robert Elias",
+                skin_color: "gold"
+            }
+        ]
+
+    }
+
+    mockGetData.mockResolvedValueOnce(someData);
+    const {getByText} = render(<StarWarsCharacters/>);
+    const next = getByText(/next/i);
+    const prev = getByText(/previous/i);
+    fireEvent.click(next);
+    fireEvent.click(prev);
+    expect(mockGetData).toHaveBeenCalledTimes(1);
+    await wait(()=> expect(getByText(/robert elias/i)));
+    
 });
-
-test('test test', ()=> {});
